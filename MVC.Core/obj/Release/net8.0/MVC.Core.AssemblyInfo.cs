08f707a3ca9c8dabe288156cceb7fc2a3fad7a06
Mostafa Hagag <moustafahagag222@gmail.com>
2025-01-22 12:52:48 +0200

    last commit

--- MVC.Core/obj/Release/net8.0/MVC.Core.AssemblyInfo.cs
+++ MVC.Core/obj/Release/net8.0/MVC.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495bf09e931c203862eb08b6868243e4b642b20b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MVC.Infrastructure/obj/Release/net8.0/MVC.Infrastructure.AssemblyInfo.cs
+++ MVC.Infrastructure/obj/Release/net8.0/MVC.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495bf09e931c203862eb08b6868243e4b642b20b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
